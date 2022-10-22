5d757e150a087cd76ce898a1a6a1c6c5549c09ac
Jaime Jose Stall Filho <jaimestall94@gmail.com>
2022-10-21 23:43:17 -0300

    requisição fetch da api responsavel pelos dados dos pokemon - aula 5

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,33 @@
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export async function getStaticProps() {
+  const maxPokemon = 50000;
+  const api = 'https://pokeapi.co/api/v2/pokemon/'
+  const res = await fetch(`${api}/?limit=${maxPokemon}`)
+  const data = await res.json()
+
+  // add pokémon index
+  data.results.forEach((item, index) => {
+    item.id = index + 1;
+  })
+  console.log(data.results)
+
+  return {
+    props: {
+      pokemon: data.results
+    },
+  }
+}
+
+export default function Home({ pokemon }) {
   return (
     <div>
       <h1>PokeNext</h1>
+      <ul>
+        {pokemon.map((pokemon) => (
+          <li key={pokemon.id}>{pokemon.name}</li>
+        ))}
+      </ul>
     </div>
   )
 }
